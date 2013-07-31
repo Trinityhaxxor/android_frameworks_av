d4fed2bbf8d768e0279f374567744be62d0190d0
RaymanFX <raymanfx@gmail.com>
2013-07-31 10:51:51 +0200

    media: fix qcom legacy inclusion

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -100,7 +100,12 @@ endif
 ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf)
 LOCAL_C_INCLUDES += \
         $(TOP)/hardware/qcom/media-caf/mm-core/inc
-else
+endif
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),legacy)
+LOCAL_C_INCLUDES += \
+        $(TOP)/hardware/qcom/media-legacy/mm-core/inc
+endif
+ifeq ($(TARGET_QCOM_MEDIA_VARIANT),)
 LOCAL_C_INCLUDES += \
         $(TOP)/hardware/qcom/media/mm-core/inc
 endif
